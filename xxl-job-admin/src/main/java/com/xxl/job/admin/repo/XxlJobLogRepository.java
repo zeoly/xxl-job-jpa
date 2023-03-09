90e9231c5c11a405f1b464504989b977d74395c0
zeoly <zeoly100@163.com>
2023-03-09 16:35:05 +0800

    fix name bug

--- xxl-job-admin/src/main/java/com/xxl/job/admin/repo/XxlJobLogRepository.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/repo/XxlJobLogRepository.java
@@ -4,6 +4,7 @@ import com.xxl.job.admin.core.model.XxlJobLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,7 @@ public interface XxlJobLogRepository extends JpaRepository<XxlJobLog, Long>, Jpa
 
     @Query("select l from XxlJobLog l left join XxlJobRegistry r on l.executorAddress = r.registryValue where l.triggerCode = 200" +
             "and l.handleCode = 0 and l.triggerTime <= :losedTime and r.id is null")
-    List<XxlJobLog> findLostJobIds(Date losedTime);
+    List<XxlJobLog> findLostJobIds(@Param("losedTime") Date losedTime);
 
     int countByTriggerTimeBetween(Date from, Date to);
 
